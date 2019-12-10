ae4adebccffb0af577b77b57b92a0568ebf0ee01
VasiliyLysokobylko <vasily@onpositive.com>
2019-12-10 19:16:14 +0700

    (fragment_tflite_add) addition "Object detection" radio button

---
+++ app/src/main/java/com/onpositive/dldemos/DetectionAsyncTask.java
@@ -0,0 +1,150 @@
+package com.onpositive.dldemos;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.AsyncTask;
+import android.util.TypedValue;
+
+import androidx.annotation.Nullable;
+
+import com.onpositive.dldemos.data.ContentType;
+import com.onpositive.dldemos.data.DetectionResultItem;
+import com.onpositive.dldemos.data.DetectionResultItemDao;
+import com.onpositive.dldemos.detection.ImageDetector;
+import com.onpositive.dldemos.tools.Logger;
+import com.onpositive.dldemos.tools.Utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.List;
+
+public class DetectionAsyncTask extends AsyncTask<ContentType, Integer, DetectionResultItem> {
+
+    private static final float FILTER_VALUE = 0.5f;
+    private static final float TEXT_SIZE_DIP = 18;
+    private static Logger log = new Logger(DetectionAsyncTask.class);
+    DetectionResultItem detectionRI = null;
+    ContentType contentType;
+    private boolean isCanceled = false;
+    private DetectionFragment fragment;
+    @Nullable
+    private ImageDetector detector;
+
+    public DetectionAsyncTask(ImageDetector detector, DetectionFragment fragment) {
+        this.detector = detector;
+        this.fragment = fragment;
+    }
+
+    @Override
+    protected DetectionResultItem doInBackground(ContentType... contentTypes) {
+        switch (contentTypes[0]) {
+            case IMAGE:
+                contentType = ContentType.IMAGE;
+                File resultImageFile = null;
+                try {
+                    List<ImageDetector.ObjectDetection> recognitions = detector.recognizeImage(BitmapFactory.decodeFile(fragment.getCurrentPhotoPath()));
+                    resultImageFile = Utils.createImageFile(fragment.getActivity());
+                    FileOutputStream stream = new FileOutputStream(resultImageFile);
+                    drawRecognitions(BitmapFactory.decodeFile(fragment.getCurrentPhotoPath()), recognitions)
+                            .compress(Bitmap.CompressFormat.JPEG, 100, stream);
+                    ;
+                    stream.flush();
+                    stream.close();
+                    String thumbnailPath = Utils.createThumbnail(fragment.getActivity(), resultImageFile.getAbsolutePath(), ContentType.IMAGE);
+                    detectionRI = new DetectionResultItem(resultImageFile.getAbsolutePath(), ContentType.IMAGE, thumbnailPath);
+                    deleteFile(new File(fragment.getCurrentPhotoPath()));
+                    log.log("Image segmented successfully. Image file path:" + resultImageFile.getAbsolutePath());
+                } catch (Exception e) {
+                    log.log("Failed image segmentation AsyncTask:\n" + e.getMessage());
+                }
+                break;
+        }
+        if (null != detectionRI)
+            saveDetectionResults(detectionRI);
+        return detectionRI;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        log.log("Object Detection Async Task onPreExecute");
+    }
+
+    @Override
+    protected void onPostExecute(DetectionResultItem detectionResultItem) {
+        super.onPostExecute(detectionResultItem);
+// TODO fix recycler view update and progress showing
+
+//        fragment.detectionResultList.add(detectionResultItem);
+//        Collections.sort(fragment.detectionResultList);
+//        Collections.reverse(fragment.detectionResultList);
+//        fragment.classifyRvAdapter.notifyDataSetChanged();
+        log.log("'onPostExecute' executed");
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        log.log("onProgressUpdate called");
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onCancelled() {
+        super.onCancelled();
+        isCanceled = isCancelled();
+        log.log("AT method 'onCancelled' executed");
+    }
+
+    private void deleteFile(File file) {
+        if (null != file && file.exists())
+            file.delete();
+        log.log("File was deleted. Is file exist: " + file.exists() +
+                "\nDeleted file path: " + file.getAbsolutePath());
+    }
+
+    private void saveDetectionResults(DetectionResultItem detectionResultItem) {
+        DetectionResultItemDao resultItemDao = MLDemoApp.getInstance().getDatabase().detectionResultItemDao();
+        detectionResultItem.setTfLiteParentFile(fragment.getTfLiteItem().getTfFilePath());
+        resultItemDao.insert(detectionResultItem);
+        log.log("Object Detection results are saved");
+    }
+
+    private Bitmap drawRecognitions(Bitmap bitmap, List<ImageDetector.ObjectDetection> recognitions) {
+        Bitmap recognitionBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Canvas canvas = new Canvas(recognitionBitmap);
+        float textSizePx =
+                TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP, TEXT_SIZE_DIP, fragment.getResources().getDisplayMetrics());
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(10.0f);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeMiter(100);
+
+        Paint textPaint = new Paint();
+        textPaint.setTextSize(textSizePx);
+        textPaint.setColor(Color.RED);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setAntiAlias(false);
+        textPaint.setAlpha(255);
+
+        for (ImageDetector.ObjectDetection recognition : recognitions) {
+            if (FILTER_VALUE > recognition.getConfidence()) {
+                continue;
+            }
+            if (recognition.getTitle().length() > 0) {
+                canvas.drawText(recognition.getTitle(),
+                        recognition.getLocation().left + textSizePx,
+                        recognition.getLocation().top + textSizePx,
+                        textPaint);
+            }
+            canvas.drawRect(recognition.getLocation(), paint);
+        }
+        return recognitionBitmap;
+    }
+}
